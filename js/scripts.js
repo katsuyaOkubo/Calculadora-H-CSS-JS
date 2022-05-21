9f2275a5ae6c1574875301b6db21be1c3009de07
Katsuya Okubo <katsuya.okubo11@gmail.com>
2022-05-21 13:23:29 -0300

    botões funcionando

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+const previousOperationText = document.querySelector('#previous-operation');
+const currentOperationText = document.querySelector('#current-operation');
+const buttons = document.querySelectorAll('#buttons-container button');
+
+class Calculator {
+
+}
+
+buttons.forEach((btn) => {
+    btn.addEventListener("click", (e) =>{
+        const value = e.target.innerText;
+
+        if(+value >= 0 || value === "."){
+            console.log(value);
+        } else {
+            console.log("Op:  " + value);
+        }
+    });
+} );
